27e1c2dac9760d8337115f296609d949d847d047
Emieldv <emielvlees@hotmail.com>
2020-04-22 16:13:10 +0200

    added test command and

--- bot.js
+++ bot.js
@@ -9,9 +9,25 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    if (msg.content === 'ping') {
-      msg.reply('pong');
-    }
-  });
+  if (msg.content === 'ping') {
+    c
+  }
+});
+
+client.on('message', msg => {
+    if (msg.content.substring(0, 1) == '!') {
+        var args = msg.content.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !test
+            case 'test':
+                msg.reply('gelukt!');;
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
+});
 
 client.login(process.env.DISCORD_TOKEN);
